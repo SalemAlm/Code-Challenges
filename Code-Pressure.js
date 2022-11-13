15076620cbcf96a0aedb30d3377a63366b125fbc
Salem Almezel <sfalmezel@outlook.com>
2022-11-13 17:35:57 -0400

    Create Code-Pressure.js

---
+++ Code-Pressure.js
@@ -0,0 +1,11 @@
+// function doubleInteger(i) {
+//     // i will be an integer. Double it and return it.
+//     return i;
+//   }
+
+//solution 
+
+function doubleInteger(i) {
+    i = i * 2
+    return (i)
+}
