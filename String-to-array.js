4e0eb99f12525fe758b07da55a39197cef6c961c
Salem Almezel <sfalmezel@outlook.com>
2022-11-18 18:28:56 -0400

    Create String-to-array.js
    completed nov 18th 2022

---
+++ String-to-array.js
@@ -0,0 +1,15 @@
+// Write a function to split a string and convert it into an array of words.
+
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+//my solution 
+
+//psuedocode: 
+
+
+
+function stringToArray(string) {
+    return string.split(' ');
+}
